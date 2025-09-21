d1b81266d7914540faec2147e1d7a6836444a0d1
Anton Johansson <antonproxykonto@gmail.se>
2025-09-21 23:59:16 +0200

    Fungerande kod.

--- wordfreq.py
+++ wordfreq.py
@@ -1,20 +1,33 @@
 def tokenize(lines):
         if isinstance(lines, str):
-                lines = lines.lower()
-                lines = lines.split()
+                lines = lines.lower().split()
         
         words = []
-
         for line in lines:
+
                 start = 0
-                while start < len(line) and line[start].isspace():
-                        start += 1
+                while start < len(line):
+                        if start < len(line) and line[start].isspace():
+                                while start < len(line) and line[start].isspace():
+                                        start += 1
+                                words.append(line[word_start:start])
+                        
+                        elif start < len(line) and line[start].isalpha():
+                                word_start = start
+                                while start < len(line) and line[start].isalpha():
+                                        start += 1
+                                words.append(line[word_start:start])
 
-                                
-        print(words)
-        start = start + 1
+                        elif start < len(line) and line[start].isdigit():
+                                word_start = start
+                                while start < len(line) and line[start].isdigit():
+                                        start += 1
+                                words.append(line[word_start:start])
 
+                        else:
+                                words.append(line[start:start + 1])                                
+                                start += 1    
         return words
 
 
-tokenize("Hej,jag heter Anton!")
+print(tokenize("Hej,jag heter Anton!"))
--- wordfreq_save.py
+++ wordfreq_save.py
@@ -1,21 +1,33 @@
 def tokenize(lines):
+        if isinstance(lines, str):
+                lines = lines.lower().split()
+        
         words = []
         for line in lines:
+
                 start = 0
                 while start < len(line):
-                        while line[start].isspace() == True:
-                                start = start + 1
-                        if line[start]:
-                                print(line[start])
-                                start = start + 1
+                        if start < len(line) and line[start].isspace():
+                                while start < len(line) and line[start].isspace():
+                                        start += 1
+                                words.append(line[word_start:start])
+                        
+                        elif start < len(line) and line[start].isalpha():
+                                word_start = start
+                                while start < len(line) and line[start].isalpha():
+                                        start += 1
+                                words.append(line[word_start:start])
+
+                        elif start < len(line) and line[start].isdigit():
+                                word_start = start
+                                while start < len(line) and line[start].isdigit():
+                                        start += 1
+                                words.append(line[word_start:start])
+
+                        else:
+                                words.append(line[start:start + 1])                                
+                                start += 1    
         return words
 
-tokenize(['apple','pi  e'])
 
-
-"""
-.lower()
-.isalpha():
-.isdigit():
-.isspace()
-"""
+print(tokenize("Hej,jag heter Anton!"))
