2e6857207266ef16773d41675f66ee64f46b8ef7
Anton Johansson <antonproxykonto@gmail.se>
2025-09-23 12:40:31 +0200

    Spacing changes.

--- topmost.py
+++ topmost.py
@@ -1,18 +1,17 @@
 import sys
 import wordfreq as w
 
+
 def main():
     # Opens and tokenize the text.
     text = open(sys.argv[2], encoding="utf-8")
     tokens = w.tokenize(text)
     text.close()
 
-
     # Open stopwords and count the frequence.
     with open(sys.argv[1], "r", encoding="utf-8") as f:
         stop_words = f.read().splitlines()
         frequencies = w.countWords(tokens, stop_words)
-
 
     # Prints the n most common words.
     n = int(sys.argv[3])
